8ce07071725f7429336cddfc92f06c15d3e0a42a
Peter Waldschmidt <peterw@gnoso.com>
2013-08-10 13:53:54 -0400

    Add JudyL implementation.

---
+++ judyl.go
@@ -0,0 +1,164 @@
+package judy
+
+/*
+#cgo LDFLAGS: -lJudy
+#include <Judy.h>
+*/
+import "C"
+
+import (
+	"math"
+	"unsafe"
+)
+
+// A JudyL array is the equivalent of a dynamic array of uint64 values. A value is addressed by an index (key).
+// The array may be sparse, and the index may be any uint64 number.
+//
+// The default value of this struct is a valid empty JudyL array.
+//
+//    j := JudyL{}
+//    defer j.Free()
+//
+//    j.Insert(5142, 142)
+//    fmt.Printf("Number of items: %v", j.CountAll())
+//
+//
+// Memory to support the array is allocated as index/value pairs are inserted, and released as index/value
+// pairs are deleted. A JudyL array can also be thought of as a mapper, that is "map" a word to another
+// word. If the JudyL array is freed ( by calling .Free() ), all words are removed (and the JudyL array requires no memory).
+// As with an ordinary array, a JudyL array contains no duplicate indexes.
+//
+// NOTE: The Judy array is implemented in C and allocates memory directly from the operating system. It is NOT
+// garbage collected by the Go runtime. It is very important that you call Free() on a Judy array after using
+// it to prevent memory leaks. The "defer" pattern is a great way to accomplish this.
+type JudyL struct {
+	array unsafe.Pointer
+}
+
+// Insert an Index and Value into the JudyL array. If the Index is successfully inserted, the Value is
+// initialized as well. If the Index was already present, the current Value is replaced with the provided Value.
+func (j *JudyL) Insert(index uint64, value uint64) {
+	pval := unsafe.Pointer(C.JudyLIns(C.PPvoid_t(&j.array), C.Word_t(index), nil))
+	*((*C.Word_t)(pval)) = C.Word_t(value)
+}
+
+// Delete the Index/Value pair from the JudyL array.
+// Returns true if successful. Returns false if Index was not present.
+func (j *JudyL) Delete(index uint64) bool {
+	return C.JudyLDel(C.PPvoid_t(&j.array), C.Word_t(index), nil) != 0
+}
+
+// Get the Value associated with Index in the Judy array
+//   returns (value, true) if the index was found
+//   returns (_, false) if the index was not found
+func (j *JudyL) Get(index uint64) (uint64, bool) {
+	pval := unsafe.Pointer(C.JudyLGet(C.Pcvoid_t(j.array), C.Word_t(index), nil))
+	if pval == nil {
+		return 0, false
+	} else {
+		return uint64(*((*C.Word_t)(pval))), true
+	}
+}
+
+// Free the entire JudyL array.
+// Return the number of bytes freed.
+//
+// NOTE: The Judy array allocates memory directly from the operating system and is NOT garbage collected by the
+// Go runtime. It is very important that you call Free() on a Judy array after using it to prevent memory leaks.
+func (j *JudyL) Free() uint64 {
+	return uint64(C.JudyLFreeArray(C.PPvoid_t(&j.array), nil))
+}
+
+// Count the number of indexes present in the JudyL array.
+// Returns the count. A return value of 0 can be valid as a count, or it can indicate a special case for fully populated array (32-bit machines only). See libjudy docs for ways to resolve this.
+func (j *JudyL) CountAll() uint64 {
+	return uint64(C.JudyLCount(C.Pcvoid_t(j.array), 0, math.MaxUint64, nil))
+}
+
+// Count the number of indexes present in the JudyL array between indexA and indexB (inclusive).
+// Returns the count. A return value of 0 can be valid as a count, or it can indicate a special case for fully populated array (32-bit machines only). See libjudy docs for ways to resolve this.
+func (j *JudyL) CountFrom(indexA, indexB uint64) uint64 {
+	return uint64(C.JudyLCount(C.Pcvoid_t(j.array), C.Word_t(indexA), C.Word_t(indexB), nil))
+}
+
+// Return the number of bytes of memory currently in use by JudyL array. This is a very fast routine,
+// and may be used with little performance impact.
+func (j *JudyL) MemoryUsed() uint64 {
+	return uint64(C.JudyLMemUsed(C.Pcvoid_t(j.array)))
+}
+
+// Search (inclusive) for the first index present that is equal to or greater than the passed index.
+// (Start with index = 0 to find the first index in the array.) This is typically used to begin a sorted-order scan of the indexes present in a JudyL array.
+//
+//   index - search index
+//   returns uint64 - value of the first index that is equal to or greater than the passed index
+//                    (only if bool return value is true)
+//           uint64 - value pointed to by the index
+//           bool   - true if the search was successful, false if an index was not found
+func (j *JudyL) First(index uint64) (uint64, uint64, bool) {
+	idx := C.Word_t(index)
+	pval := unsafe.Pointer(C.JudyLFirst(C.Pcvoid_t(j.array), &idx, nil))
+
+	if pval == nil {
+		return 0, 0, false
+	} else {
+		return uint64(idx), uint64(*((*C.Word_t)(pval))), true
+	}
+}
+
+// Search (exclusive) for the first index present that is greater than the passed index.
+// This is typically used to continue a sorted-order scan of the indexes present in a JudyL array.
+//
+//   index - search index
+//   returns uint64 - value of the first index that is greater than the passed index
+//                    (only if bool return value is true)
+//           uint64 - value pointed to by the index
+//           bool   - true if the search was successful, false if an index was not found
+func (j *JudyL) Next(index uint64) (uint64, uint64, bool) {
+	idx := C.Word_t(index)
+	pval := unsafe.Pointer(C.JudyLNext(C.Pcvoid_t(j.array), &idx, nil))
+
+	if pval == nil {
+		return 0, 0, false
+	} else {
+		return uint64(idx), uint64(*((*C.Word_t)(pval))), true
+	}
+}
+
+// Search (inclusive) for the last index present that is equal to or less than than the passed index.
+// (Start with index = math.MaxUint64 to find the last index in the array.) This is typically used to begin a reverse-sorted-order scan of the indexes present in a JudyL array.
+//
+//   index - search index
+//   returns uint64 - value of the last index that is equal to or less than the passed index
+//                    (only if bool return value is true)
+//           uint64 - value pointed to by the index
+//           bool   - true if the search was successful, false if an index was not found
+func (j *JudyL) Last(index uint64) (uint64, uint64, bool) {
+	idx := C.Word_t(index)
+	pval := unsafe.Pointer(C.JudyLLast(C.Pcvoid_t(j.array), &idx, nil))
+
+	if pval == nil {
+		return 0, 0, false
+	} else {
+		return uint64(idx), uint64(*((*C.Word_t)(pval))), true
+	}
+}
+
+// Search (exclusive) for the last index present that is less than the passed index.
+// This is typically used to continue a reverse sorted-order scan of the indexes present in a JudyL array.
+//
+//   index - search index
+//   returns uint64 - value of the last index that is less than the passed index
+//                    (only if bool return value is true)
+//           uint64 - value pointed to by the index
+//           bool   - true if the search was successful, false if an index was not found
+func (j *JudyL) Prev(index uint64) (uint64, uint64, bool) {
+	idx := C.Word_t(index)
+	pval := unsafe.Pointer(C.JudyLPrev(C.Pcvoid_t(j.array), &idx, nil))
+
+	if pval == nil {
+		return 0, 0, false
+	} else {
+		return uint64(idx), uint64(*((*C.Word_t)(pval))), true
+	}
+}
---
+++ judyl_test.go
@@ -0,0 +1,327 @@
+package judy
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+)
+
+func TestEmptyJudyLArray(t *testing.T) {
+
+	j := JudyL{}
+	r := j.Free()
+
+	if r != 0 {
+		t.Errorf("Free should return 0, returned %v", r)
+	}
+}
+
+func TestJudyLCount(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	if ct := j.CountAll(); ct != 0 {
+		t.Errorf("Count should be zero, was %v", ct)
+	}
+
+	var i uint64
+	for i = 0; i < 100; i++ {
+		j.Insert(i, i)
+	}
+
+	var ct uint64
+	if ct = j.CountAll(); ct != 100 {
+		t.Errorf("Count should be 100, was %v", ct)
+	}
+
+	if ct = j.CountFrom(0, 1000); ct != 100 {
+		t.Errorf("Count should be 100, was %v", ct)
+	}
+	if ct = j.CountFrom(200, 1000); ct != 0 {
+		t.Errorf("Count should be 0, was %v", ct)
+	}
+	if ct = j.CountFrom(5, 5); ct != 1 {
+		t.Errorf("Count should be 1, was %v", ct)
+	}
+	if ct = j.CountFrom(20, 29); ct != 10 {
+		t.Errorf("Count should be 10, was %v", ct)
+	}
+
+}
+
+func TestJudyLInsertGet(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	var i uint64
+	for i = 0; i < 100; i++ {
+		j.Insert(i*100000, i)
+	}
+
+	for i = 0; i < 100; i++ {
+		if v, ok := j.Get(i * 100000); ok && v != i {
+			t.Errorf("Index %v should be %v, but was %v", i*100000, i, v)
+		}
+	}
+
+	for i = 1; i < 100; i++ {
+		if _, ok := j.Get(i * 99999); ok {
+			t.Errorf("Index %v incorrectly set", i*99999)
+		}
+	}
+
+}
+
+func TestJudyLDelete(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	j.Insert(12345, 234)
+	j.Insert(11235, 11235)
+	j.Insert(54321, 4321)
+
+	if ct := j.CountAll(); ct != 3 {
+		t.Errorf("Count should be 3")
+	}
+	if ok := j.Delete(11235); !ok {
+		t.Errorf("Delete should return ok")
+	}
+	if ct := j.CountAll(); ct != 2 {
+		t.Errorf("Count should be 2")
+	}
+	if _, ok := j.Get(11235); ok {
+		t.Errorf("Value should be removed")
+	}
+	if ok := j.Delete(11235); ok {
+		t.Errorf("Delete not should return ok")
+	}
+
+}
+
+func TestJudyLFirst(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	var i uint64
+	for i = 0; i < 100; i++ {
+		j.Insert(i*2, i)
+	}
+
+	if next, val, ok := j.First(20); ok && (next != 20 || val != 10) {
+		t.Errorf("First(20) should be 20, 10 was %v,%v", next, val)
+	}
+	if next, val, ok := j.First(21); ok && (next != 22 || val != 11) {
+		t.Errorf("First(21) should be 22, 11 was %v,%v", next, val)
+	}
+	if _, _, ok := j.First(201); ok {
+		t.Errorf("First(201) should not be found")
+	}
+
+}
+
+func TestJudyLLast(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	var i uint64
+	for i = 1; i < 100; i++ {
+		j.Insert(i*2, i)
+	}
+
+	if next, val, ok := j.Last(20); ok && (next != 20 || val != 10) {
+		t.Errorf("Last(20) should be 20,10 was %v,%v", next, val)
+	}
+	if next, val, ok := j.Last(21); ok && (next != 20 || val != 10) {
+		t.Errorf("Last(21) should be 20, 10 was %v,%v", next, val)
+	}
+	if _, _, ok := j.Last(1); ok {
+		t.Errorf("Last(1) should not be found")
+	}
+}
+
+func TestJudyLNext(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	var i uint64
+	for i = 0; i < 100; i++ {
+		j.Insert(i*2, i)
+	}
+
+	if next, val, ok := j.Next(20); ok && (next != 22 || val != 11) {
+		t.Errorf("Next(20) should be 22,11 was %v,%v", next, val)
+	}
+	if next, val, ok := j.Next(21); ok && (next != 22 || val != 11) {
+		t.Errorf("Next(21) should be 22,11 was %v,%v", next, val)
+	}
+	if _, _, ok := j.Next(200); ok {
+		t.Errorf("Next(200) should not be found")
+	}
+
+}
+
+func TestJudyLPrev(t *testing.T) {
+
+	j := JudyL{}
+	defer j.Free()
+
+	var i uint64
+	for i = 1; i < 100; i++ {
+		j.Insert(i*2, i)
+	}
+
+	if next, val, ok := j.Prev(20); ok && (next != 18 || val != 9) {
+		t.Errorf("Prev(20) should be 18,9 was %v,%v", next, val)
+	}
+	if next, val, ok := j.Prev(21); ok && (next != 20 || val != 10) {
+		t.Errorf("Prev(21) should be 20,10 was %v", next, val)
+	}
+	if _, _, ok := j.Prev(2); ok {
+		t.Errorf("Prev(2) should not be found")
+	}
+
+}
+
+func runOrderedJudyLMemUsageTest(t *testing.T, n int) {
+	j := JudyL{}
+	defer j.Free()
+
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(i*10000), uint64(i))
+	}
+
+	if ct := j.CountAll(); int(ct) != n {
+		t.Errorf("Count should be %v, was %v", n, ct)
+	}
+	t.Logf("Memory Usage with %7v ordered bits %8v", n, j.MemoryUsed())
+}
+
+func runRandomJudyLMemUsageTest(t *testing.T, n int) {
+	j := JudyL{}
+	defer j.Free()
+
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(rand.Int63()), uint64(rand.Int63()))
+	}
+
+	if ct := j.CountAll(); int(ct) != n {
+		t.Errorf("Count should be %v, was %v", n, ct)
+	}
+	t.Logf("Memory Usage with %7v random bits  %8v", n, j.MemoryUsed())
+}
+
+func TestMemUsage(t *testing.T) {
+
+	runOrderedJudyLMemUsageTest(t, 1000)
+	runRandomJudyLMemUsageTest(t, 1000)
+	runOrderedJudyLMemUsageTest(t, 10000)
+	runRandomJudyLMemUsageTest(t, 10000)
+	runOrderedJudyLMemUsageTest(t, 100000)
+	runRandomJudyLMemUsageTest(t, 100000)
+	runOrderedJudyLMemUsageTest(t, 1000000)
+	runRandomJudyLMemUsageTest(t, 1000000)
+
+	//t.Fail() // Uncomment to see the log output
+}
+
+func BenchmarkJudyLCountAllRand1000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(rand.Int63()), uint64(rand.Int63()))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountAll(); int(ct) != n {
+			b.Errorf("Count should be %v, was %v", n, ct)
+		}
+	}
+}
+
+func BenchmarkJudyLCountAllRand1000000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(rand.Int63()), uint64(rand.Int63()))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountAll(); int(ct) != n {
+			b.Errorf("Count should be %v, was %v", n, ct)
+		}
+	}
+}
+
+func BenchmarkJudyLCountAllOrd1000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(i), uint64(i))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountAll(); int(ct) != n {
+			b.Errorf("Count should be %v, was %v", n, ct)
+		}
+	}
+}
+
+func BenchmarkJudyLCountAllOrd1000000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(i), uint64(i))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountAll(); int(ct) != n {
+			b.Errorf("Count should be %v, was %v", n, ct)
+		}
+	}
+}
+
+func BenchmarkJudyLCountRangeRand1000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(rand.Int63()), uint64(rand.Int63()))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountFrom(math.MaxUint64/8, (math.MaxUint64/8)*7); int(ct) < n/2 {
+			b.Errorf("Count should > %v, was %v", n/2, ct)
+		}
+	}
+}
+
+func BenchmarkJudyLCountRangeRand1000000(b *testing.B) {
+	j := JudyL{}
+	defer j.Free()
+
+	n := 1000000
+	for i := 0; i < n; i++ {
+		j.Insert(uint64(rand.Int63()), uint64(rand.Int63()))
+	}
+
+	for loops := 0; loops < b.N; loops++ {
+		if ct := j.CountFrom(math.MaxUint64/8, (math.MaxUint64/8)*7); int(ct) < n/2 {
+			b.Errorf("Count should > %v, was %v", n/2, ct)
+		}
+	}
+}
